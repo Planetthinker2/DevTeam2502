cb1527e6d72883f36bf5ccc052df73652340857a
Aaron L. Begley Jr <albegley@student.fullsail.edu>
2025-02-05 01:35:23 -0500

    Player movement  Added
    Player movement has been created and implemented.
    
    CharacterController created and attached to player object
    
    Camera controller attached to player but no controls added yet.

---
+++ FPS DEV Group/Assets/Scripts/Player.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] CharacterController controller;
+    [SerializeField] int speed = 1;
+    [SerializeField] bool canRun = true;
+
+    Vector3 moveDir; 
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();   
+    }
+    void Movement()
+    {
+
+        moveDir = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        controller.Move(moveDir * speed * Time.deltaTime);
+        
+        /*if(canRun && Input.GetButtonDown("LeftShift"))  
+        else
+        {
+        controller.Move(moveDir * speed * Time.deltaTime);
+
+        }*/
+
+    }
+
+}
