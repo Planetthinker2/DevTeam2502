0186dcaeb5a1962f3cc80b722192146cfebaa141
Aaron L. Begley Jr <albegley@student.fullsail.edu>
2025-02-05 20:02:36 -0500

    Final commit before merge
    commit to ensure continuity between branches. Changes were made on Main not on here

--- FPS DEV Group/Assets/Scripts/Player.cs
+++ FPS DEV Group/Assets/Scripts/Player.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [Header("- - - - - - - - Player Setting Variables - - - - - - - -")]
     [SerializeField] CharacterController controller;
     [SerializeField] int speed = 1;
     [SerializeField] bool canRun = true;
