add85a2480f4e5d7eb2ee37bb69e7fb78c33cc6f
ayushdhakal1 <159081182+ayushdhakal1@users.noreply.github.com>
2025-02-10 17:55:46 -0600

    Player movement updated
    Sprint and jump is added to player movement

--- FPS DEV Group/Assets/Scripts/playerController.cs
+++ FPS DEV Group/Assets/Scripts/playerController.cs
@@ -12,7 +12,22 @@ public class playerController : MonoBehaviour
 
     [Tooltip("Speed at which the player moves (in units per second.)")]
     [SerializeField] int speed;
+
+    [SerializeField] int sprintMod;
+
+    [SerializeField] int jumpSpeed;
+
+    [SerializeField] int jumpMax;
+
+    [SerializeField] int gravity;
+
+    int jumpCount;
+    
     Vector3 moveDir;
+
+    Vector3 playerVel;
+
+    bool isSprinting;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -23,16 +38,50 @@ public class playerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-     movement();
+        movement();
+        sprint();
     }
 
     void movement()
     {
+        if (controller.isGrounded)
+        {
+            jumpCount = 0;
+            playerVel =Vector3.zero;
+        }
         //moveDir = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
         //transform.position += moveDir * speed * Time.deltaTime; 
 
         moveDir = (Input.GetAxis("Horizontal") * transform.right) + 
                   (Input.GetAxis("Vertical") * transform.forward);
         controller.Move(moveDir * speed * Time.deltaTime);
+
+        jump();
+
+        controller.Move(playerVel * speed * Time.deltaTime);
+        playerVel.y -= gravity * Time.deltaTime;
+
     } 
+
+    void sprint()
+    {
+        if(Input.GetButtonDown("Sprint")) 
+        {
+            speed *= sprintMod;
+        }
+        else if (Input.GetButtonUp("Sprint"))
+        {
+            speed /= sprintMod;
+        }
+    }
+
+    void jump()
+    {
+        if (Input.GetButtonDown("Jump") && jumpCount < jumpMax)
+        {
+            jumpCount++;
+            playerVel.y = jumpSpeed * Time.deltaTime;
+
+        }
+    }
 }
